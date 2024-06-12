71363c38aa4dcd2c61185405e453901207c58bff
danipopp <popp.daniel16@icloud.com>
2024-06-12 20:45:32 +0200

    DataToPredict

---
+++ DecisionTree.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+
+class TreeNode:
+    pass
+
+class DecisionTree:
+    pass
+    
+if __name__ == '__main__':
+    iris = load_iris()
+    X, y = iris.data, iris.target
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Decision-Tree
 Here I tried to make my first implementation for an decision tree
+
+
