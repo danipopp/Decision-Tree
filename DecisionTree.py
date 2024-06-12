789917f937c297531fcc05e0b6893a4ad14468fb
danipopp <popp.daniel16@icloud.com>
2024-06-12 21:01:17 +0200

    Preparation

--- DecisionTree.py
+++ DecisionTree.py
@@ -4,7 +4,19 @@ class TreeNode:
     pass
 
 class DecisionTree:
-    pass
+
+    def gini_impurty(y):
+        pass
+
+    def get_information(actual_impurty,left,right):
+        pass
+
+    def split(X,y):
+        pass
+
+    def generate_tree(X,y,max_depth=10):
+        # TODO
+        pass
     
 if __name__ == '__main__':
     iris = load_iris()
